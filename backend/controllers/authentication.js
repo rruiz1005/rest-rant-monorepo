db21da14d8933f70126b7d2f7c107feb203f6b06
Ryan Ruiz <ryan.hans1176@gmail.com>
2022-08-06 11:56:13 -0400

    handling log

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -9,6 +9,14 @@ router.post('/', async (req, res) => {
     let user = await User.findOne ({
         where: { email: req.body.email }
     })
+    
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({
+            message:'Could not find a user with the provided username and password'
+        })
+    } else {
+        res.json({ user })
+    }
 
     console.log(user)
 })
